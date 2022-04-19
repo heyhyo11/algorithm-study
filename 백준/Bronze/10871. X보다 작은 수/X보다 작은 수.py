8813a03f890f2a372dd5398c4fbd7b2e3cbb5da2
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2022-04-20 02:16:21 +0900

    [Bronze III] Title: X보다 작은 수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -2,11 +2,7 @@ import sys
 
 a, b = map(int, sys.stdin.readline().split())
 num_list = list(map(int, sys.stdin.readline().split()))
-new_list = []
 
 for i in range(a):
   if num_list[i] < b:
-    new_list.append(num_list[i])
-
-for j in range(len(new_list)):
-  print(new_list[j], end=' ')
+    print(num_list[i], end=' ')
