fbe8125ca595ad491f95b9ff24134bbf2047dc19
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2022-04-21 03:11:58 +0900

    [Bronze I] Title: 단어 공부, Time: 104 ms, Memory: 32796 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,15 +1,13 @@
-import sys
+words = input().upper()
+unique_words = list(set(words))  # 입력받은 문자열에서 중복값을 제거
 
-str_list = list(sys.stdin.readline().rstrip().upper())
-set_list = list(set(str_list))
-cnt_list = [0 for i in range(len(set_list))]
+cnt_list = []
+for x in unique_words :
+    cnt = words.count(x)
+    cnt_list.append(cnt)  # count 숫자를 리스트에 append
 
-for i in range(len(set_list)):
-  cnt_list[i] = str_list.count(set_list[i])
-
-max_ = max(cnt_list)
-
-if len(list(filter(lambda x: x == max_, cnt_list))) > 1:
-  print("?")
-else:
-  print(set_list[cnt_list.index(max_)])
+if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+    print('?')
+else :
+    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
+    print(unique_words[max_index])
