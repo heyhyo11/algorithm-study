918493293f9689bd9ee60dd4358126cd4f1aec18
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2022-04-21 04:21:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,15 +2,8 @@ import sys
 
 str_list = sys.stdin.readline().rstrip()
 alpha_list = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-cnt_list = []
 
-for alpha in alpha_list:
-  cnt = str_list.count(alpha)
-  cnt_list.append(cnt)
-  
-cnt_list[7] -= cnt_list[2]
-others = len(str_list) - sum(cnt_list)*2 - cnt_list[2]
+for i in alpha_list:
+  str_list = str_list.replace(i, "*")
 
-# ans = other + sum(cnt_list) 이거 풀어쓴 것
-ans = len(str_list) - sum(cnt_list) - cnt_list[2]
-print(ans)
+print(len(str_list))
