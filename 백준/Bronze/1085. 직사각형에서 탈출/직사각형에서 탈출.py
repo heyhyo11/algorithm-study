471ffd96ab9c1c7447014567cf8544b425c57f26
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2022-04-24 04:18:49 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -3,9 +3,5 @@ import sys
 # 1. 입력 받기
 x, y, w, h = map(int, sys.stdin.readline().rstrip().split())
 
-# 2. x, y축 최소거리 구하기
-x_distance = min(w-x, x)
-y_distance = min(h-y, y)
-
 # 3. 최종 최소거리 구하기
-print(min(x_distance, y_distance))
+print(min(x, y, w-x, h-y))
