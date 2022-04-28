9302fa1ba47026ebdb584cf70ba087d8a0f3d402
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2022-04-28 17:55:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -2,11 +2,9 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())
 
-def fibonacci(N):
-  if N == 0:
-    return 0
-  elif N == 1:
-    return 1
-  return fibonacci(N-2) + fibonacci(N-1)
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
 
 print(fibonacci(N))
