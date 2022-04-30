af19b6f59a4e121589d46c123a8ded2c4c43e25f
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2022-05-01 02:14:03 +0900

    [Silver V] Title: 소인수분해, Time: 1512 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11653. 소인수분해/소인수분해.py
+++ 백준/Silver/11653. 소인수분해/소인수분해.py
@@ -1,14 +1,30 @@
+## 나의 풀이
+# import sys
+
+# N = int(sys.stdin.readline().rstrip())
+# num_list = []
+# cnt = 2
+
+# while N >= cnt:
+#   if N % cnt == 0:
+#     num_list.append(cnt)
+#     N = N / cnt
+#   else:
+#     cnt += 1
+    
+# print(*num_list, sep='\n')
+
+
+## 다른 사람의 풀이
+# 참고사이트: https://leedakyeong.tistory.com/entry/%EB%B0%B1%EC%A4%80-11653%EB%B2%88-%EC%86%8C%EC%9D%B8%EC%88%98%EB%B6%84%ED%95%B4-in-python-%ED%8C%8C%EC%9D%B4%EC%8D%AC
 import sys
 
 N = int(sys.stdin.readline().rstrip())
-num_list = []
 cnt = 2
 
-while N >= cnt:
+while N != 1:
   if N % cnt == 0:
-    num_list.append(cnt)
     N = N / cnt
+    print(cnt)
   else:
     cnt += 1
-    
-print(*num_list, sep='\n')
