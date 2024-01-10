1b5477a9a0f5f0b368546a3da32f91e77c041139
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-01-11 00:41:05 +0900

    [Gold V] Title: 괄호의 값, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,38 +1,55 @@
-# 참고 블로그: https://pottatt0.tistory.com/entry/%EB%B0%B1%EC%A4%80-2504-%ED%95%B4%EC%84%A4-python-%EA%B4%84%ED%98%B8%EC%9D%98-%EA%B0%92
-
 import sys
+
 input = sys.stdin.readline
-arr = input().rstrip()
+string = input().rstrip()
+
+ans = 0
+num = 1
 stack = []
-cnt = 1
-res = 0
-for i in range(len(arr)):
-    if arr[i] == '(':
-        stack.append(arr[i])
-        cnt *= 2
-    elif arr[i] == '[':
-        stack.append(arr[i])
-        cnt *= 3
-    elif arr[i] == ')':
+
+for i in range(len(string)):
+
+    # 괄호가 열릴 때마다 곱할 수를 정한다.
+    if string[i] == '(':
+        num *= 2
+        stack.append(string[i])
+
+    elif string[i] == '[':
+        num *= 3
+        stack.append(string[i])
+
+    # 괄호가 닫힐 때마다 정답에 수를 더해준다.  
+    elif string[i] == ')':
+
+        # 이상한 괄호 순서일 경우 ans=0 처리해준다.
+        # stack의 마지막이 [ 이거나 stack이 비어있는 경우
         if stack == [] or stack[-1] == '[':
-            print(0)
+            ans = 0
             break
-        else:
-            if arr[i-1] =='(':
-                res+=cnt
+
+        # () 인 경우 stack에서 마지막을 제거하고 ans에 더한다.
+        # 더 이상 그 괄호 안에 들어갈 수 없으므로 num에도 괄호의 수를 나눈다.
+        else: 
+            if string[i-1] == '(':
+                ans += num
+
+            # 현재 ) 이고 stack의 마지막이 ], [ 인 경우
+            # 괄호가 정상적으로 닫혔으므로 곱해주었던 수를 다시 나눠서 처음으로.
             stack.pop()
-            cnt //= 2
-    elif arr[i] == ']':
+            num //= 2
+    
+    elif string[i] == ']':
         if stack == [] or stack[-1] == '(':
-            print(0)
+            ans = 0
             break
-        else:
-            if arr[i-1] =='[':
-                res+=cnt
+        else: 
+            if string[i-1] == '[':
+                ans += num
             stack.pop()
-            cnt //= 3
+            num //= 3
+
+if stack:
+    print(0)
+
 else:
-    if stack:
-        print(0)
-    else:
-        print(res)
+    print(ans)
