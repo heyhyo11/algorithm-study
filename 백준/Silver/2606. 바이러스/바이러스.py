2c5f219c8c295b68ad23dd3809485f1fa5113cb8
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-01-11 11:52:30 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,18 +1,38 @@
-from sys import stdin
-read = stdin.readline
-dic={}
-for i in range(int(read())):
-    dic[i+1] = set()
-for j in range(int(read())):
-    a, b = map(int,read().split())
-    dic[a].add(b)
-    dic[b].add(a)
+# 참고 블로그: https://hei-jayden.tistory.com/103
 
-def dfs(start, dic):
-    for i in dic[start]:
-        if i not in visited:
-            visited.append(i)
-            dfs(i, dic)
-visited = []
-dfs(1, dic)
-print(len(visited)-1)
+from collections import deque
+
+n = int(input())
+connect = int(input())
+
+# graph: n대의 컴퓨터가 각 연결된 컴퓨터를 확인할 수 있음
+graph = [[] for i in range(n+1)]
+visited = [False]*(n+1)
+cnt = 0
+
+for i in range(connect):
+    s, e = map(int, input().split())
+    # 네트워크 상 연결 : 양방향
+    graph[s].append(e)
+    graph[e].append(s)
+
+def bfs(graph, v):
+    global cnt
+    # bfs는 deque 안의 리스트 사용
+    queue = deque([v])
+
+    # bfs는 deque 안이 전부 사라질 때까지 반복
+    while queue:
+        # 가장 왼쪽의 요소부터 pop() 진행
+        pop = queue.popleft()
+        visited[pop] = True
+
+        for i in graph[pop]:
+            if visited[i]==False:
+                visited[i] = True
+                # 방문한 컴퓨터와 연결된 부분을 큐 안에 추가
+                queue.append(i)
+                cnt += 1 
+    print(cnt)
+
+bfs(graph, 1)
