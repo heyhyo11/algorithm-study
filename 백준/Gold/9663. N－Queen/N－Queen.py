9cb69ae829ada3aa15459e6e5235ec8f9e919fa4
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-02-28 19:44:24 +0900

    [Gold IV] Title: N-Queen, Time: 6780 ms, Memory: 120284 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,35 +1,22 @@
-## 참고사이트: https://developnote.tistory.com/70
+# 풀이 참고
 
-import sys
+def dfs(n):
+    global ans
+    if n==N:    # 마지막 N행까지 진행하면 
+        ans+=1
+        return
 
-def logic(n):
-    if n == N: # 마지막까지 탐색완료
-        global count # 전역변수 설정
-        count += 1
-    else:
-        for i in range(N):
-            if visited[i]:
-                continue
-            board[n] = i # (n, i)에 퀸 올리기
-            
-            if check(n): # 퀸 놓기 조건에 맞다면
-                visited[i] = True
-                logic(n+1) # 다음행으로 넘어가기
-                visited[i] = False
+    for j in range(N): # 모든 행을 돌린다
+        if v1[j]==v2[n+j]==v3[n-j]==0:  # 같은 열, 대각선에 없을 경우에만 진행
+            # v1: 열, v2: 우측 대각선, v3: 좌측 대각선
+            v1[j] = v2[n + j] = v3[n - j] = 1 # 방문 표시
+            dfs(n+1) # 다음 행으로 진행
+            v1[j] = v2[n + j] = v3[n - j] = 0 # 원래대로 되돌리기
 
-def check(n):
-    for i in range(n):
-        # 중복된 자리거나, 대각선 상에 있는지 확인
-        # (행끼리의 차 == 열끼리의 차의 절대값)이 True면 대각선상에 있는 것
-        if (board[n] == board[i]) or (n-i == abs(board[n]-board[i])):
-            return False
-    return True
-
-if __name__ == '__main__':
-    N = int(sys.stdin.readline())
-    count = 0
-    board = [0 for _ in range(N)] # 인덱스번호: 행, 인덱스값: 열
-    visited = [False for _ in range(N)]
-    
-    logic(0)
-    print(count)
+N = int(input())
+ans = 0
+v1 = [0]*N
+v2 = [0]*(2*N)
+v3 = [0]*(2*N)
+dfs(0)
+print(ans)
--- 백준/Gold/9663. N－Queen/README.md
+++ 백준/Gold/9663. N－Queen/README.md
@@ -1,14 +1,18 @@
-# [Gold V] N-Queen - 9663 
+# [Gold IV] N-Queen - 9663 
 
 [문제 링크](https://www.acmicpc.net/problem/9663) 
 
 ### 성능 요약
 
-메모리: 146676 KB, 시간: 12944 ms
+메모리: 120284 KB, 시간: 6780 ms
 
 ### 분류
 
-백트래킹(backtracking), 브루트포스 알고리즘(bruteforcing)
+백트래킹, 브루트포스 알고리즘
+
+### 제출 일자
+
+2024년 2월 28일 19:44:06
 
 ### 문제 설명
 
