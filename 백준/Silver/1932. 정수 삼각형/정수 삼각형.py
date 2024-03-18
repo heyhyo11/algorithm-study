35d48e7b73e741ad653ab40cb0a583c5619bc58c
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-03-18 15:56:07 +0900

    [Silver I] Title: 정수 삼각형, Time: 132 ms, Memory: 35616 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,17 +1,21 @@
-## 참고사이트: https://pacific-ocean.tistory.com/148
+import sys
 
+input = sys.stdin.readline
 n = int(input())
-t = []
-for i in range(n):
-    t.append(list(map(int, input().split())))
+ans = []
 k = 2
+
+for i in range(n):
+    ans.append(list(map(int, input().split())))
+
 for i in range(1, n):
     for j in range(k):
         if j == 0:
-            t[i][j] = t[i][j] + t[i - 1][j]
+            ans[i][j] = ans[i][j] + ans[i-1][j]
         elif i == j:
-            t[i][j] = t[i][j] + t[i - 1][j - 1]
+            ans[i][j] = ans[i][j] + ans[i-1][j-1]
         else:
-            t[i][j] = max(t[i - 1][j - 1], t[i - 1][j]) + t[i][j]
+            ans[i][j] = max(ans[i-1][j-1], ans[i-1][j]) + ans[i][j]
     k += 1
-print(max(t[n - 1]))
+    
+print(max(ans[n-1]))
