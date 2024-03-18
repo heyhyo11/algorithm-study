91080425342a1a46460dcd634349c64edb913aa4
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-03-18 16:03:52 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,14 +1,17 @@
-# 참고사이트: https://velog.io/@hope1213/%EB%B0%B1%EC%A4%80-1149-RGB%EA%B1%B0%EB%A6%AC-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+# 풀이 참고
 
+import sys
+
+input = sys.stdin.readline
 n = int(input())
-a = [0]*n
+arr = [0]*n
 
 for i in range(n):
-    a[i] = list(map(int,input().split()))
+    arr[i] = list(map(int,input().split()))
     
-for i in range(1,n): # 1부터 시작하는 이유는 다음 입력값이 이전 입력값의 최소값을 사용하기때문이다
-    a[i][0]= min(a[i-1][1],a[i-1][2]) + a[i][0]
-    a[i][1]= min(a[i-1][0],a[i-1][2]) + a[i][1]
-    a[i][2]= min(a[i-1][0],a[i-1][1]) + a[i][2]
+for i in range(1,n):
+    arr[i][0]= min(arr[i-1][1], arr[i-1][2]) + arr[i][0]
+    arr[i][1]= min(arr[i-1][0], arr[i-1][2]) + arr[i][1]
+    arr[i][2]= min(arr[i-1][0], arr[i-1][1]) + arr[i][2]
 
-print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
+print(min(arr[n-1][0], arr[n-1][1], arr[n-1][2]))
