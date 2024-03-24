9ed0917c4538d6523fd2f3414d170033145795da
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-03-25 03:36:46 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,19 +1,25 @@
-N, K = map(int, input().split()) 
+import sys
 
+input = sys.stdin.readline
 coins = []
+n, k = map(int, input().split()) 
 
-for _ in range(N):
+# 동전 넣기
+for _ in range(n):
     coins.append(int(input()))
     
+# 동전 정렬 (내림차순)   
 coins.sort(reverse=True)
 
-answer = 0
+# 정답
+ans = 0
 
+# 큰 액수부터 돌면서 계산
 for coin in coins:
-    if K >= coin:
-        answer += K // coin
-        K %= coin
-        if K <= 0: 
+    if k >= coin:
+        ans += k // coin # 동전 개수
+        k %= coin # 남은 금액
+        if k <= 0: # 계산이 끝나면 중간에 나오기
        		break
             
-print(answer) 
+print(ans) 
