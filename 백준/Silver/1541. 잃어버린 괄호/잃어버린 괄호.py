2f752bbb126613d738dcac099e07630683800315
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-03-25 04:14:26 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,25 +1,19 @@
 import sys
 
-# 입력 받기 / - 기준으로 분리
-# 예시) 20 - (10 + 80) - (20 + 70)
-expression = sys.stdin.readline().rstrip().split('-')
-num_list = []
+input = sys.stdin.readline
+mark = input().strip().split('-')
+nums = []
 
-# + 기준으로 분리
-for i in expression:
-  cnt = 0
-  plus = i.split('+')
-  
-  # + 덩어리들 더하기
-  for j in plus:
-    cnt += int(j)
-  
-  num_list.append(cnt)
 
-# 첫 번째 요소에서 + 덩어리들을 - 해준다.   
-num = num_list[0]
+for i in mark:
+    cnt = 0
+    plus = list(map(int, i.split('+')))
+    cnt += sum(plus)
+    nums.append(cnt)
 
-for i in range(1, len(num_list)):
-  num -= num_list[i]
-  
+num = nums[0]
+
+for i in range(1, len(nums)):
+    num -= nums[i]
+
 print(num)
