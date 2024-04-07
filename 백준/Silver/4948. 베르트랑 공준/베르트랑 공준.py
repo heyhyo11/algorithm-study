c1c8c5f1d00a089946f18b2953813431f96f6cd2
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-04-08 04:04:26 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1116 ms, Memory: 40216 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,26 +1,28 @@
-def sosu(n):
-    if n ==1:
+import sys
+input = sys.stdin.readline
+
+
+all_nums = list(range(2, 246912))
+check = []
+
+def prime_numbers(n):
+    if n == 1:
         return False
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
-    return True							#소수 구하는 방식은 위와 같다
+    return True	
 
-all_list = list(range(2,246912))		#문제에서 제한한 범위
-memo = []								#for문 밖에 리스트 정의
-
-for i in all_list:						#2부터 2*123,456 범위
-    if sosu(i):							#sosu함수에 해당하면
-        memo.append(i)					#리스트에 추가
-
-n = int(input())
+for i in all_nums:
+    if prime_numbers(i):
+        check.append(i)
 
 while True:
-    count=0					#갯수를 세야하는 조건 때문에 카운트
-    if n == 0 :
-            break
-    for i in memo:			#memo리스트 중에서
-        if n < i <=2*n:		#입력한 값의 범위 내에서 값이 있으면
-            count+=1		#있을 때 마다 카운트 +1
-    print(count)
-    n = int(input())		#0 입력받기 전까지 계속 해야하므로 입력 받음
+    n = int(input())
+    cnt = 0
+    if n == 0:
+        break
+    for i in check:
+        if n < i <= 2*n:
+            cnt += 1
+    print(cnt)
