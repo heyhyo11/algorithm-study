7fd59a3e271fbb369ca65a45f2aeaff143d3861e
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-05-24 23:42:52 +0900

    [level 2] Title: 더 맵게, Time: 1877.83 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -6,11 +6,12 @@ def solution(scoville, K):
     min_sum = 0
     cnt = 0
     
-    while scoville[0] < K:
+    while min_sum < K:
         try:
             min1 = heapq.heappop(scoville)
             min2 = heapq.heappop(scoville)
             heapq.heappush(scoville, min1 + min2*2)
+            min_sum = scoville[0]
             cnt += 1
         except:
             return -1
