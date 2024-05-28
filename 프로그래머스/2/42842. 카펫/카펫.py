5d01e74385ebc61d78ebbb92a623968b8d1041bf
Hyojung Nam <96091519+heyhyo11@users.noreply.github.com>
2024-05-29 01:36:09 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,4 +1,17 @@
 def solution(brown, yellow):
-    brown = (brown - 4) // 2
-    yellow_list = [i for i in range(1, brown) if i * (brown - i) == yellow]
-    return [max(yellow_list) + 2, min(yellow_list) + 2]
+    
+    # 근의 공식으로 풀이
+    a = 1
+    b = -(brown-4)//2
+    c = yellow
+    
+    Yx = (-b+(b**2-4*a*c)**(1/2)) // 2
+    Yy = yellow // Yx
+    
+    return [Yx+2, Yy+2]
+    
+    
+    # 예전 풀이
+    # brown = (brown - 4) // 2
+    # yellow_list = [i for i in range(1, brown) if i * (brown - i) == yellow]
+    # return [max(yellow_list) + 2, min(yellow_list) + 2]
